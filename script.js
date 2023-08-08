2c702dd56cee261a356f24d4da54baf26613fb18
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-08 16:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = ""; // Your Result goes here
   // Only change code below this line
-	for(let i-0;i<encodedStr.length;i++){
+	for(let i=0;i<encodedStr.length;i++){
 		if(encodedStr[i]>='A' && encodedStr[i]<='Z'){
 			decodedStr+=lookup.encodedStr[i];
 		}
