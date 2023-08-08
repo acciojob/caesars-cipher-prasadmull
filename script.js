2aa225569adf1215b29d76d1bdec60f5363f2742
Prasad Mullamuri <119662453+prasadmull@users.noreply.github.com>
2023-08-08 16:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,13 +32,23 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  let decodedArr = ""; // Your Result goes here
   // Only change code below this line
+	for(let i-0;i<encodedStr.length;i++){
+		if(encodedStr[i]>='A' && encodedStr[i]<='Z'){
+			decodedStr+=lookup.encodedStr[i];
+		}
+		else{
+			decodedStr+=encodedStr[i];
+		}
+	}
+	
 
-  return; //return decodedArr
+  return decodedStr; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+console.log("hello Prasad!")
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
 
